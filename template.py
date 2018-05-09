1e9c476068cddc39ab7d7e2fba62828a89eeeb1e
Geoff Goldrick <geoff.goldrick@det.nsw.edu.au>
2018-05-10 08:45:47 +1000

    Add files via upload
    Added python template. TODO rename this to hangman.py

---
+++ template.py
@@ -0,0 +1,12 @@
+""" Short, one line description of the module ending with a period.
+A longer description of the module with details that may help the user or anybody
+reviewing the code later. make sure you outline in detail what the module does and how it can be used.
+"""
+
+__author__ = "Your Name"
+__copyright__ = "Copyright 2018, NSW Dept. Ed."
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Your Name again unless you are cheating"
+__email__ = "your.addressk@education.nsw.com.au"
+__status__ = "Prototype, Development or Production"
